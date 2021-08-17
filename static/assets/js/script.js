af82e03edf5d3b788bb86d4b93d16a842eb2f3d2
SRay-2020 <stephen.r.ray@gmail.com>
2021-08-17 14:46:43 +0000

    Add new Ireland map and begin linking accordian section

--- static/assets/js/script.js
+++ static/assets/js/script.js
@@ -3,3 +3,8 @@ var dropdownList = dropdownElementList.map(function (dropdownToggleEl) {
   return new bootstrap.Dropdown(dropdownToggleEl)
 })
 
+document.querySelector('input[name="mapbutton"]').onclick = function() {
+  var image = document.getElementById('iremap');
+  image.style.width = '25%';
+  image.style.height = 'auto';
+}
