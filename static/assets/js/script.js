6359d0e243e8dffe91b767aa0b0d067fe642b91e
SRay-2020 <stephen.r.ray@gmail.com>
2021-08-17 17:12:21 +0000

    Add expand sections beneath maps and gave inline svg properties

--- static/assets/js/script.js
+++ static/assets/js/script.js
@@ -3,8 +3,17 @@ var dropdownList = dropdownElementList.map(function (dropdownToggleEl) {
   return new bootstrap.Dropdown(dropdownToggleEl)
 })
 
-document.querySelector('input[name="mapbutton"]').onclick = function() {
-  var image = document.getElementById('iremap');
-  image.style.width = '25%';
-  image.style.height = 'auto';
-}
+
+// CLICK ATTEMPT 1
+
+// document.querySelector('input[name="mapbutton"]').onclick = function() {
+//   var image = document.getElementById('iremap');
+//   image.style.width = '25%';
+//   image.style.height = 'auto';
+// }
+
+// CLICK ATTEMPT 2
+
+// $("#button").click(function() {
+//   $('.transform').toggleClass('transform-active');
+// });
